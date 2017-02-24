0dd3b6ed08ac0d2199daed9fb91cbd461d5fcfbb
mcpengelly <pengelly.mat@gmail.com>
2017-02-23 21:02:07 -0500

    playing with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router history={hashHistory}>
+          <Route path='/' component={Home} />
+          <Route path='/address' component={Address} />
+        </Router>
         <div className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
@@ -17,5 +23,9 @@ class App extends Component {
     );
   }
 }
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
+
 
 export default App;
