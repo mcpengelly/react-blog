6fefa2015ffd6ec4c0ae14874bedc01ae6875b8f
mcpengelly <pengelly.mat@gmail.com>
2017-02-26 11:30:37 -0500

    add background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';import './App.css';
 
 import NavBar from './components/utility/NavBar';
+import backgroundImage from './assets/img/small_steps.png';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
+				<div className="App" style={{
+					backgroundImage: "url(" + backgroundImage + ")"
+				}}>
 				<div className="App-header">
 					<NavBar />
 					<img src={logo} className="App-logo" alt="logo" />
