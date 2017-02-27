7e869d385ae2833a2b98544ebf8272f70df9c7c1
mcpengelly <pengelly.mat@gmail.com>
2017-02-27 16:04:42 -0500

    playing around with react-icons

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,18 +7,7 @@ export default class Portfolio extends Component {
 			<div>
 				<h1>Portfolio Page</h1>
 				<h2>Programming</h2>
-				<Grid>
-					<Row>
-						<Col xs={2} md={3}>Hello, world!</Col>
-						<Col xs={2} md={3}>Hello, world!</Col>
-					</Row>
-					<Row>
-						<Col xs={12} sm={3} md={2} lg={1} />
-						<Col xs={6} sm={6} md={8} lg={10} />
-						<Col xs={6} sm={3} md={2} lg={1} />
-					</Row>
-					<Col xs={6} md={3}>Hello, world!</Col>
-				</Grid>
+
 				<h2>Other</h2>
 			</div>
 		);
--- src/components/utility/Footer.js
+++ src/components/utility/Footer.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import A from './A';
 
-//TODO: load an icon for the external links
+//load icons
+import FaGithub from 'react-icons/lib/fa/github';
+import FaLinkedIn from 'react-icons/lib/fa/linkedin';
+import FaTwitter from 'react-icons/lib/fa/twitter';
+
 export default class Footer extends Component {
 	render(){
 		return (
 			<footer style={{ backgroundColor: 'grey' }}>
-				<A linkTo="https://github.com/mcpengelly">Github</A>
-				<A linkTo="https://www.linkedin.com/in/matt-pengelly-575ba886">LinkedIn</A>
-				<A linkTo="https://twitter.com/typycyl">Twitter</A>
+				<A linkTo="https://github.com/mcpengelly"><FaGithub /></A>
+				<A linkTo="https://www.linkedin.com/in/matt-pengelly-575ba886"><FaLinkedIn /></A>
+				<A linkTo="https://twitter.com/typycyl"><FaTwitter /></A>
 				<small><p style={{ display: 'inline-block' }}>{this.props.copyright}</p></small>
 			</footer>
 		);
 	}
 };
+
 
 
