f61c99f0cd3d27507a3c7a90caf99a4bec85aa85
mcpengelly <pengelly.mat@gmail.com>
2017-03-04 17:48:39 -0500

    include projects.js

---
+++ src/components/data/Projects.js
@@ -0,0 +1,24 @@
+const projectsList = [
+	{
+		title: 'project',
+		description: 'placeholder',
+		img: ''
+	},
+	{
+		title: 'project',
+		description: 'placeholder2',
+		img: ''
+	},
+	{
+		title: 'project',
+		description: 'placeholder3',
+		img: ''
+	},
+	{
+		title: 'project',
+		description: 'placeholder4',
+		img: ''
+	}
+];
+
+export default projectsList;
