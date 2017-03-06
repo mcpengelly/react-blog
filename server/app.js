2f3afab580b8e54c9d98bb6e961d8dbe0cccc02d
mcpengelly <pengelly.mat@gmail.com>
2017-03-06 15:51:50 -0500

    start implementing projects api

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-// server/app.js
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -7,8 +6,10 @@ const mailer = require('nodemailer');
 
 const app = express();
 
+// MIDDLEWARE
 // Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version"' +
+	':status :res[content-length] :response-time ms'));
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
@@ -16,16 +17,73 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 // req body middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// APIs
+
+const pg = require('pg');
+var pgClient = new pg.Client({
+	user: 'matthewpengelly',
+	password: 'postgres',
+	database: 'mydb',
+	host: 'localhost',
+	port: 5432
 });
 
+// Projects API
+// GET list of projects
+app.get('/api/projects', (req, res) => {
+	pgClient.connect((err) => {
+		pgClient.query('SELECT * FROM projects', (err, result) => {
+			if (err) {
+				res.status(500);
+				throw err;
+			}
 
+			// disconnect
+			pgClient.end((err) => {
+				if (err) {
+					throw err;
+				}
+				res.status(200);
+				if(result && result.rows) {
+					res.send(result.rows);
+				} else {
+					res.send('no projects in the database');
+				}
+			});
+		});
+	});
+});
 
+// CREATE new project
+app.post('/api/projects', (req, res) => {
+	// authenticate?
+	pgClient.connect((err) => {
+		if (err) {
+			res.status(500);
+			throw err;
+		}
+		const querystring = `INSERT INTO projects VALUES ('${req.query.title}, ${req.query.description}')`;
+		pgClient.query(querystring, (err, result) => {
+			if (err) {
+				res.status(500);
+				throw err;
+			}
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
+			// disconnect
+			pgClient.end((err) => {
+				if (err) {
+					throw err;
+				}
+
+				res.status(200);
+				res.send(result.rows);
+			});
+		});
+	});
+});
+
+// Mailer
+const transporter = mailer.createTransport({
 	service: 'gmail',
 	auth: {
 		user: 'gmail.user@gmail.com',
@@ -33,23 +91,30 @@ let transporter = nodemailer.createTransport({
 	}
 });
 
-app.post('/send-mail', (req, res) => {
+app.post('/api/send-mail', (req, res) => {
 	// setup email data with unicode symbols
-	let mailOptions = {
+	const mailOptions = {
 		from: '"Burna" <burner@gmail.com>', // sender address
 		to: 'pengelly.mat@gmail.com',
-		subject: 'Hello from: ' + req.body.email,
-		text: req.body.name + ' has sent you: ' + req.body.message,
-		html: '<b>Hello world ?</b>
+		subject: `Hello from: ${req.body.email}`,
+		text: `${req.body.name} has sent you: ${req.body.message}`
 	};
 
 	// send mail with defined transport object
 	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
-			return console.log(error);
+			console.log(error);
 		}
 		console.log('Message %s sent: %s', info.messageId, info.response);
 	});
+	res.status(200);
+	res.send('email sent');
+});
+
+// REACT
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+
 'use strict';
 
 const app = require('./app');
@@ -6,5 +7,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+	console.log(`App listening on port ${PORT}!`);
 });
