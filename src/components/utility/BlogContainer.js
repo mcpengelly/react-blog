000e3a6bcfbbbc08475d1645d5da21fe938d4b86
mcpengelly <pengelly.mat@gmail.com>
2017-03-10 20:24:59 -0500

    remove ugly css

--- src/components/utility/BlogContainer.js
+++ src/components/utility/BlogContainer.js
@@ -5,15 +5,9 @@ import BlogPost from './BlogPost';
 //TODO: import list of projects from a .json/.js file
 export default class BlogContainer extends Component {
 	render(){
-		const style = {
-			margin: '10px',
-			borderRadius: '10px',
-			backgroundColor:'white'
-		};
-
 		let blogPosts = this.props.posts.map((post, index) => {
 			return (
-				<div key={index} style={style}>
+				<div key={index}>
 					<BlogPost title={post.title} body={post.body} />
 				</div>
 			);
--- src/components/utility/SideBar.js
+++ src/components/utility/SideBar.js
@@ -10,21 +10,8 @@ import MeImage from '../../assets/img/me.png';
 export default class SideBar extends Component {
 	render() {
 		return (
-			<Col className="sticky-sidebar" sm={3}>
-				<div style={{
-					margin: '10px',
-					position: 'fixed',
-					borderRadius: '10px',
-					backgroundColor: 'lightgrey'
-				}}>
-					<Row style={{padding: '10px'}}>
-						<ProfileImage source={MeImage}></ProfileImage>
-					</Row>
-
-					<Row style={{padding: '10px'}}>
-						<SocialMediaIcons />
-					</Row>
-
+			<Col sm={3}>
+				<div>
 					<Row>
 						<p>Latest Posts</p>
 						<p>Most Viewed</p>
