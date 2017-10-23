fb94f414d783519aea99eaeb9a00c34f1f12eb1a
mcpengelly <pengelly.mat@gmail.com>
2017-10-23 16:42:39 -0400

    cleaning up

--- server/index.js
+++ server/index.js
@@ -8,17 +8,16 @@ const app = require('./app');
 const HTTP_PORT = process.env.PORT || 9000;
 const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
-app.listen(HTTP_PORT, 'localhost', () => {
+app.listen(HTTP_PORT, () => {
 	console.log(`App listening on port ${HTTP_PORT}!`);
 });
 
-// var sslOptions = {
-// 	key: fs.readFileSync(__dirname + '/ssl/server.key'),
-// 	cert: fs.readFileSync(__dirname + '/ssl/server.cert')
-// };
+// Local https
+if(process.env.NODE_ENV === 'dev'){
+	var sslOptions = {
+		key: fs.readFileSync(__dirname + '/ssl/server.key'),
+		cert: fs.readFileSync(__dirname + '/ssl/server.cert')
+	};
 
-// https.createServer(sslOptions, app).listen(HTTPS_PORT);
-
-// Prod
-// if(process.env.NODE_ENV === 'production'){
-// }
+	https.createServer(sslOptions, app).listen(HTTPS_PORT);
+}
