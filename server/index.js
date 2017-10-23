37b7ab86049af63ee25c495eabb497f2511b65d1
mcpengelly <pengelly.mat@gmail.com>
2017-10-23 16:07:11 -0400

    trying https thru nginx

--- server/index.js
+++ server/index.js
@@ -8,16 +8,16 @@ const app = require('./app');
 const HTTP_PORT = process.env.PORT || 9000;
 const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
-app.listen(HTTP_PORT, () => {
+app.listen(HTTP_PORT, 'localhost', () => {
 	console.log(`App listening on port ${HTTP_PORT}!`);
 });
 
-var sslOptions = {
-	key: fs.readFileSync(__dirname + '/ssl/server.key'),
-	cert: fs.readFileSync(__dirname + '/ssl/server.cert')
-};
+// var sslOptions = {
+// 	key: fs.readFileSync(__dirname + '/ssl/server.key'),
+// 	cert: fs.readFileSync(__dirname + '/ssl/server.cert')
+// };
 
-https.createServer(sslOptions, app).listen(HTTPS_PORT);
+// https.createServer(sslOptions, app).listen(HTTPS_PORT);
 
 // Prod
 // if(process.env.NODE_ENV === 'production'){
