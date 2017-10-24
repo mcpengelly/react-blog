20a70e2d333760890682c0bc55287c8dc7dedf33
mcpengelly <pengelly.mat@gmail.com>
2017-10-24 19:57:55 -0400

    [light-refactoring] more refactoring

--- server/routes.js
+++ server/routes.js
@@ -47,37 +47,64 @@ passport.use(
 );
 
 module.exports = function(app) {
-	/**
-		projects api
-	 */
-	// GET list of projects
-	app.get('/api/projects', (req, res) => {
-		db
-			.any('SELECT * from projects')
-			.then(projects => {
-				if (projects.length < 1) {
-					res.send('no projects found');
-				} else {
-					res.send(projects);
-				}
-			})
-			.catch(err => {
-				res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-			});
-	});
 
-	// GET project by id
-	app.get('/api/projects/:id', (req, res) => {
-		db
-			.one('SELECT * from projects WHERE id = $1', [req.params.id])
-			.then(post => {
-				//TODO: return different message when nothing found?
-				res.send(post);
-			})
-			.catch(err => {
-				res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-			});
-	});
+	function getAll(relation){
+		app.get(`/api/${relation}`, (req, res) => {
+			db
+				.any(`SELECT * from ${relation}`)
+				.then(data => {
+					if (data.length < 1) {
+						res.send('no ${relation} found');
+					} else {
+						res.send(data);
+					}
+				})
+				.catch(err => {
+					res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
+				});
+		});
+	}
+
+	function getOneById(relation){
+		app.get(`/api/${relation}/:id`, (req, res) => {
+			db
+				.one(`SELECT * from ${relation} WHERE id = $1`, [req.params.id])
+				.then(data => {
+					//TODO: return different message when nothing found?
+					res.send(data);
+				})
+				.catch(err => {
+					res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
+				});
+		});
+	}
+
+	function deleteById(relation){
+		app.delete(
+			`/api/${relation}/:id`,
+			passport.authenticate('basic', {
+				session: false
+			}),
+			(req, res) => {
+				db
+					.none(`DELETE FROM ${relation} WHERE id = $1`, [req.params.id])
+					.then(() => {
+						res.send(`${relation} ID: ${req.params.id} has been deleted`);
+					})
+					.catch(err => {
+						res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
+					});
+			}
+		);
+	}
+
+	getAll('projects');
+	getAll('posts');
+	getOneById('projects');
+	getOneById('posts');
+	deleteById('projects');
+	deleteById('posts');
+
 
 	// CREATE new project
 	app.post(
@@ -130,54 +157,9 @@ module.exports = function(app) {
 		}
 	);
 
-	// DELETE existing project using id
-	app.delete(
-		'/api/projects/:id',
-		passport.authenticate('basic', {
-			session: false
-		}),
-		(req, res) => {
-			db
-				.none('DELETE FROM projects WHERE id = $1', [req.params.id])
-				.then(() => {
-					res.send(`projects ID: ${req.params.id} has been deleted`);
-				})
-				.catch(err => {
-					res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-				});
-		}
-	);
-
 	/**
 		blog posts api
 	*/
-	// GET list of blog posts
-	app.get('/api/posts', (req, res) => {
-		db
-			.any('SELECT * FROM posts')
-			.then(posts => {
-				if (posts.length < 1) {
-					res.send('no posts found');
-				} else {
-					res.send(posts);
-				}
-			})
-			.catch(err => {
-				res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-			});
-	});
-
-	app.get('/api/posts/:id', (req, res) => {
-		db
-			.one('SELECT * from posts WHERE id = $1', [req.params.id])
-			.then(post => {
-				//TODO: return different message when nothing found?
-				res.send(post);
-			})
-			.catch(err => {
-				res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-			});
-	});
 
 	// CREATE new blog post
 	app.post(
@@ -214,24 +196,6 @@ module.exports = function(app) {
 				.none(querystring, [req.body.title, req.body.content, req.params.id])
 				.then(() => {
 					res.send(`updated post: ${req.params.id}`);
-				})
-				.catch(err => {
-					res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
-				});
-		}
-	);
-
-	// DELETE existing blog post using id
-	app.delete(
-		'/api/posts/:id',
-		passport.authenticate('basic', {
-			session: false
-		}),
-		(req, res) => {
-			db
-				.none('DELETE FROM posts WHERE id = $1', [req.params.id])
-				.then(() => {
-					res.send(`post ID: ${req.params.id} has been deleted`);
 				})
 				.catch(err => {
 					res.status(HTTP_INTERNAL_SERVER_ERROR).send(err);
