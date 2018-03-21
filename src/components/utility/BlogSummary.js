0e439e5221181183773b81b7fe4c09e02c1c8f9b
mcpengelly <pengelly.mat@gmail.com>
2018-03-21 17:14:39 -0400

    [upgrades/various] start on some new features

---
+++ src/components/utility/BlogSummary.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+
+const styles = theme => ({
+  card: {
+    minWidth: 275,
+    maxWidth: 875,
+    margin: 'auto',
+    padding: 10
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  media: {
+    height: 200
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+    color: theme.palette.text.secondary
+  },
+  pos: {
+    marginBottom: 12,
+    color: theme.palette.text.secondary
+  }
+})
+
+function BlogSummary (props) {
+  const { classes } = props
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image='/static/images/cards/contemplative-reptile.jpg'
+        title='bang'
+      />
+      <CardContent>
+        <Typography className={classes.title}>Word of the Day</Typography>
+        <Typography variant='headline'>{props.title}</Typography>
+        <Typography className={classes.pos}>adjective</Typography>
+        <Typography variant='body'>
+          {props.content}
+          <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>
+          <a href={'/' + props.id}>See More...</a>
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+BlogSummary.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(BlogSummary)
---
+++ src/components/utility/EditableBlogPost.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import TextField from 'material-ui/TextField'
+import Button from 'material-ui/Button'
+
+class EditableBlogPost extends Component {
+  constructor () {
+    super()
+    this.state = {
+      title: '',
+      content: ''
+    }
+  }
+
+  onHandleChange (name) {
+    return e => {
+      this.setState({
+        [name]: e.target.value
+      })
+    }
+  }
+
+  onSubmitClick (e) {
+    e.preventDefault()
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        title: this.state.title,
+        content: this.state.content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    // send users email data to backend
+    fetch('/api/posts', options)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        // whats our response?
+        console.log(text)
+
+        // clear inputs
+        this.setState({ title: '', content: '' })
+
+        // navigate home
+        // location.href = 'https://localhost:3000/'
+      })
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.onSubmitClick}>
+        <TextField
+          label='Blog Post Title'
+          value={this.state.title}
+          onChange={this.onHandleChange('title')}
+        />
+        <TextField
+          label='Blog Post Content'
+          multiline
+          rows='8'
+          value={this.state.content}
+          onChange={this.onHandleChange('content')}
+        />
+        <Button
+          onClick={this.addNewSubscriberNotification}
+          variant='raised'
+          color='primary'
+        >
+          Submit
+        </Button>
+      </form>
+    )
+  }
+}
+
+export default EditableBlogPost
