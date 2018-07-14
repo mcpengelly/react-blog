cfaf57bf82e7a8b92ec0e58f6274b33529d7db1d
mcpengelly <pengelly.mat@gmail.com>
2018-07-14 18:18:35 -0400

    [debt/restructuring] more mobx additions

--- src/components/utility/LoginForm.js
+++ src/components/utility/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { observer } from 'mobx-react'
 import Paper from 'material-ui/Paper'
+import AuthStore from '../stores/authentication-store'
+
 import { withStyles } from 'material-ui/styles'
 const styles = {
   container: {
@@ -9,82 +12,84 @@ const styles = {
     margin: 'auto'
   }
 }
-
-class LoginForm extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      username: '',
-      password: ''
+const LoginForm = observer(
+  class LoginForm extends React.Component {
+    constructor () {
+      super()
+      this.state = {
+        username: '',
+        password: ''
+      }
+      this.handleChange = this.handleChange.bind(this)
+      this.login = this.login.bind(this)
+      this.logout = this.logout.bind(this)
+      this.authStore = new AuthStore()
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.login = this.login.bind(this)
-    this.logout = this.logout.bind(this)
-  }
-  handleChange (inputField) {
-    return e => {
-      this.setState({
-        [inputField]: e.currentTarget.value
-      })
-    }
-  }
-  clearInputFields () {
-    this.setState({
-      username: '',
-      password: ''
-    })
-  }
-  login () {
-    const data = {
-      username: this.state.username,
-      password: this.state.password
-    }
-
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(data),
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
+    handleChange (inputField) {
+      return e => {
+        this.setState({
+          [inputField]: e.currentTarget.value
+        })
       }
     }
+    clearInputFields () {
+      this.setState({
+        username: '',
+        password: ''
+      })
+    }
+    login () {
+      const data = {
+        username: this.state.username,
+        password: this.state.password
+      }
 
-    fetch('/api/login', options)
-      // .then(res => {
-      //   // dispatch an action changing isLoggedIn app state to true
-      // })
-      .then(this.clearInputFields.bind(this))
-  }
-  logout () {
-    fetch('/logout').then(res => {
-      console.log('res', res)
-    })
-  }
-  render () {
-    const { classes } = this.props
-    const { username, password } = this.state
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
 
-    return (
-      <Paper className={classes.container}>
-        <div>
-          username:<input
-            type='text'
-            value={username}
-            onChange={this.handleChange('username')}
-          />
-          <br />
-          password:<input
-            type='text'
-            value={password}
-            onChange={this.handleChange('password')}
-          />
-          <br />
-          <input type='button' value='login' onClick={this.login} />
-          <input type='button' value='logout' onClick={this.logout} />
-        </div>
-      </Paper>
-    )
+      fetch('/api/login', options)
+        // .then(res => {
+        //   // dispatch an action changing isLoggedIn app state to true
+        // })
+        .then(this.clearInputFields.bind(this))
+    }
+    logout () {
+      fetch('/logout').then(res => {
+        console.log('res', res)
+      })
+    }
+    render () {
+      const { username, password } = this.state
+      const { classes } = this.props
+
+      return (
+        <Paper className={classes.container}>
+          <div>
+            username:<input
+              type='text'
+              value={username}
+              onChange={this.handleChange('username')}
+            />
+            <br />
+            password:<input
+              type='text'
+              value={password}
+              onChange={this.handleChange('password')}
+            />
+            <br />
+            <input type='button' value='login' onClick={this.login} />
+            <input type='button' value='logout' onClick={this.logout} />
+          </div>
+        </Paper>
+      )
+    }
   }
-}
+)
 
 export default withStyles(styles)(LoginForm)
--- src/components/utility/NavBar.js
+++ src/components/utility/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
@@ -13,6 +14,7 @@ import { FormControlLabel, FormGroup } from 'material-ui/Form'
 import Menu, { MenuItem } from 'material-ui/Menu'
 import Icon from 'material-ui/Icon'
 import FaGithub from 'react-icons/lib/fa/github'
+import AuthStore from '../stores/authentication-store'
 
 const styles = {
   root: {
@@ -27,170 +29,177 @@ const styles = {
   }
 }
 
-class MenuAppBar extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      auth: false,
-      anchorEl: null,
-      anchorNavEl: null
+const MenuAppBar = observer(
+  class MenuAppBar extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+        auth: false,
+        anchorEl: null,
+        anchorNavEl: null
+      }
+
+      this.handleChange = this.handleChange.bind(this)
+      this.handleClose = this.handleClose.bind(this)
+      this.handleMenu = this.handleMenu.bind(this)
+      this.handleNavMenu = this.handleNavMenu.bind(this)
+      this.handleNavClose = this.handleNavClose.bind(this)
+
+      this.authStore = new AuthStore()
     }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleClose = this.handleClose.bind(this)
-    this.handleMenu = this.handleMenu.bind(this)
-    this.handleNavMenu = this.handleNavMenu.bind(this)
-    this.handleNavClose = this.handleNavClose.bind(this)
-  }
+    componentDidMount () {
+      if (document.cookie) {
+        this.setState({
+          auth: true
+        })
+      } else {
+        this.setState({
+          auth: false
+        })
+      }
+    }
 
-  componentDidMount () {
-    if (document.cookie) {
-      this.setState({
-        auth: true
-      })
-    } else {
-      this.setState({
-        auth: false
-      })
+    handleChange (event, checked) {
+      this.authStore.toggleLoggedIn()
+      // this.setState({ auth: checked })
+    }
+
+    handleMenu (event) {
+      this.setState({ anchorEl: event.currentTarget })
+    }
+
+    handleClose () {
+      this.setState({ anchorEl: null })
+    }
+
+    handleNavMenu (event) {
+      this.setState({ anchorNavEl: event.currentTarget })
+    }
+
+    handleNavClose () {
+      this.setState({ anchorNavEl: null })
+    }
+
+    navigateLogin () {
+      window.location.href = '/login'
+    }
+
+    render () {
+      const { classes } = this.props
+
+      const { auth, anchorEl, anchorNavEl } = this.state
+      const openProfile = Boolean(anchorEl)
+      const openNavigation = Boolean(anchorNavEl)
+
+      return (
+        <div className={classes.root}>
+          <button onClick={this.handleChange.bind(this)}>log</button>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={this.authStore.isLoggedIn}
+                  onChange={this.handleChange}
+                  aria-label='LoginSwitch'
+                />
+              }
+              label={auth ? 'Logout' : 'Login'}
+              onClick={this.navigateLogin}
+            />
+          </FormGroup>
+          <AppBar position='static'>
+            <Toolbar>
+              <IconButton
+                className={classes.menuButton}
+                color='inherit'
+                aria-label='Menu'
+                onClick={this.handleNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <a href='https://github.com/mcpengelly'>
+                <FaGithub size={28} />
+              </a>
+              <Menu
+                id='menu-appbar'
+                anchorEl={anchorNavEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={openNavigation}
+                onClose={this.handleNavClose}
+              >
+                <MenuItem
+                  component={Link}
+                  onClick={this.handleNavClose}
+                  to='/blog'
+                >
+                  <Icon>home</Icon> - Home
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  onClick={this.handleNavClose}
+                  to='/about'
+                >
+                  <Icon>info</Icon> - About
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  onClick={this.handleNavClose}
+                  to='/portfolio'
+                >
+                  <Icon>folder</Icon> - Projects
+                </MenuItem>
+              </Menu>
+              <Typography
+                variant='title'
+                color='inherit'
+                className={classes.flex}
+              >
+                {this.state.pageTitle}
+              </Typography>
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-owns={openProfile ? 'menu-appbar' : null}
+                    aria-haspopup='true'
+                    onClick={this.handleMenu}
+                    color='inherit'
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id='menu-appbar'
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    open={openProfile}
+                    onClose={this.handleClose}
+                  >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+      )
     }
   }
-
-  handleChange (event, checked) {
-    this.setState({ auth: checked })
-  }
-
-  handleMenu (event) {
-    this.setState({ anchorEl: event.currentTarget })
-  }
-
-  handleClose () {
-    this.setState({ anchorEl: null })
-  }
-
-  handleNavMenu (event) {
-    this.setState({ anchorNavEl: event.currentTarget })
-  }
-
-  handleNavClose () {
-    this.setState({ anchorNavEl: null })
-  }
-
-  navigateLogin () {
-    window.location.href = '/login'
-  }
-
-  render () {
-    const { classes } = this.props
-    const { auth, anchorEl, anchorNavEl } = this.state
-    const openProfile = Boolean(anchorEl)
-    const openNavigation = Boolean(anchorNavEl)
-
-    return (
-      <div className={classes.root}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={auth}
-                onChange={this.handleChange}
-                aria-label='LoginSwitch'
-              />
-            }
-            label={auth ? 'Logout' : 'Login'}
-            onClick={this.navigateLogin}
-          />
-        </FormGroup>
-        <AppBar position='static'>
-          <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color='inherit'
-              aria-label='Menu'
-              onClick={this.handleNavMenu}
-            >
-              <MenuIcon />
-            </IconButton>
-            <a href='https://github.com/mcpengelly'>
-              <FaGithub size={28} />
-            </a>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorNavEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
-              }}
-              open={openNavigation}
-              onClose={this.handleNavClose}
-            >
-              <MenuItem
-                component={Link}
-                onClick={this.handleNavClose}
-                to='/blog'
-              >
-                <Icon>home</Icon> - Home
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                onClick={this.handleNavClose}
-                to='/about'
-              >
-                <Icon>info</Icon> - About
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                onClick={this.handleNavClose}
-                to='/portfolio'
-              >
-                <Icon>folder</Icon> - Projects
-              </MenuItem>
-            </Menu>
-            <Typography
-              variant='title'
-              color='inherit'
-              className={classes.flex}
-            >
-              {this.state.pageTitle}
-            </Typography>
-            {auth && (
-              <div>
-                <IconButton
-                  aria-owns={openProfile ? 'menu-appbar' : null}
-                  aria-haspopup='true'
-                  onClick={this.handleMenu}
-                  color='inherit'
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id='menu-appbar'
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  open={openProfile}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </div>
-    )
-  }
-}
+)
 
 MenuAppBar.propTypes = {
   classes: PropTypes.object.isRequired
